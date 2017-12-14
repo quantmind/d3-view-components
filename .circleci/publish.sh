06afbbc3009522524e2e5588330d3ceb0b8c729b
Luca Sbardella <luca@quantmind.com>
2017-12-14 09:10:52 +0000

    test publish

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -4,5 +4,5 @@ cd .. && git clone git@github.com:quantmind/d3-view-components.git gh-pages
 cd gh-pages && git checkout -b gh-pages origin/gh-pages
 rm -rf *
 cp -a ../build/. .
-git add * && git commit -a -m "new release [ci skip]" && git push origin gh-pages
+git add * && git commit -a -m "new site release [ci skip]" && git push origin gh-pages
 cd .. && rm -rf gh-pages
