16fb9e69a13bc24764de32e1eb63c4dd932c6de2
Luca Sbardella <luca@quantmind.com>
2017-12-14 14:46:59 +0000

    fixing pages

--- .circleci/authenticate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-git config --global user.email "bot@quantmind.com"
-git config --global user.username "qmbot"
-git config --global user.name "Quantmind Bot"
-cd && echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
