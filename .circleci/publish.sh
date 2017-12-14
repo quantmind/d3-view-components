67cef5a5889268f43e2b0e99cad3607fdfb350ae
Luca Sbardella <luca@quantmind.com>
2017-12-14 14:45:17 +0000

    remove .tmp

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -2,6 +2,7 @@ cd .. && git clone git@github.com:quantmind/d3-view-components.git gh-pages
 cd gh-pages && git checkout -b gh-pages origin/gh-pages
 rm -rf *
 cp -a ../website/build/. .
+rm -rf .tmp
 git add * && git commit -a -m "new site release [ci skip]" && git push origin gh-pages
 cd .. && rm -rf gh-pages
 cd website
