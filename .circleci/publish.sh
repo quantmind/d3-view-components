cd54ef81fcc54e924279385cf6f3f07882147765
Luca Sbardella <luca@quantmind.com>
2017-12-22 17:48:38 +0000

    new version

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -1,6 +1,8 @@
 cd .. && git clone git@github.com:quantmind/d3-view-components.git gh-pages
 cd gh-pages && git checkout -b gh-pages origin/gh-pages
+mv CNAME .CNAME
 rm -rf *
+mv .CNAME CNAME
 cp -a ../website/build/. .
 rm -rf .tmp
 git add * && git commit -a -m "new site release [ci skip]" && git push origin gh-pages
