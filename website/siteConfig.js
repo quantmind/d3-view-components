66f1edbce5a1960c60449263631906fdf769eb28
Luca Sbardella <luca@quantmind.com>
2018-01-18 20:53:16 +0000

    bump

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -10,7 +10,7 @@ module.exports = {
         paths: [
             {
                 slug: "docs",
-                path: "docs/",
+                path: "../src",
                 template: "sidenav",
                 context: {
 
